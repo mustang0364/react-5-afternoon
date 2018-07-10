581e8cc4743d8a60a283b842af4b6dd3a3609f25
abraham martinez <abrahammartinez@abrahams-MacBook-Pro.local>
2018-07-09 23:53:43 -0700

    reduxOne

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {BrowserRouter} from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
+import {HashRouter} from 'react-router-dom';
+import  {Provider } from 'react-redux';
+import store from './store'
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+<BrowserRouter>
+    <Provider store={store}>
+        <HashRouter>
+            <App>  </App>
+
+        </HashRouter> 
+    </Provider>
+</BrowserRouter>
+, document.getElementById('root'));
 registerServiceWorker();
 
